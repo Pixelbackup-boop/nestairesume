f65805cec08a7a221066e6beb574e35f9731fefb
Kayes Mahmud <kayesmahmud@gmail.com>
2026-02-23 14:53:56 +0545

    fix: remove stray closing brace from builder page breaking lint

--- frontend/app/builder/page.tsx
+++ frontend/app/builder/page.tsx
@@ -329,7 +329,6 @@ function BuilderContent() {
                                 <PagedPreview ref={componentRef} scale={previewScale} />
                             </div>
 
-                            )}
                         </div>
                     )}
                 </div>
